735604c7ffe8cd9d33669c8ba6914345309de846
MrAMIR8H0OSEIN <amirhosein44044881@gmail.com>
2024-07-25 16:16:18 +0330

    add scroll event and remove click event

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ let body = document.getElementsByTagName('html')[0];
 let back = document.getElementById('back-button');
 let mainpage = document.getElementById('content');
 let menupage = document.getElementById('content-2');
+let menubarbutton = document.querySelectorAll('#menu-bar > .items');
+let items = document.querySelectorAll('#menu-content > .items');
+menubarbutton[0].classList.add('active');
+document.addEventListener('scroll',(event)=>{
+    items.forEach((el,i)=>{
+        if(i == 0){
+            if(window.pageYOffset >= 0 && window.pageYOffset < el.nextElementSibling.offsetTop){
+                menubarbutton[i].classList.add('active');
+            }else{
+                menubarbutton[i].classList.remove('active');
+            }
+        }
+        else if(i != items.length-1){
+            if(window.pageYOffset > el.offsetTop-1 && window.pageYOffset < el.nextElementSibling.offsetTop){
+                menubarbutton[i].classList.add('active');
+            }else{
+                menubarbutton[i].classList.remove('active');
+            }
+        }else{
+            if(window.pageYOffset > el.offsetTop-1){
+                menubarbutton[i].classList.add('active');
+            }else{
+                menubarbutton[i].classList.remove('active');
+            }
+        }
+    })
+})
 button.addEventListener('click',()=>{
     mainpage.style.display = 'none';
     menupage.style.display = 'block';
@@ -11,17 +38,9 @@ back.addEventListener('click',()=>{
     menupage.style.display = 'none';
     mainpage.style.display = 'block';
 })
-let menubarbutton = document.querySelectorAll('#menu-bar > .items');
 menubarbutton.forEach((el,i)=>{
     el.setAttribute('href','#menu-'+i)
-    el.addEventListener('click',()=>{
-        menubarbutton.forEach((el)=>{
-            el.classList.remove('active');
-        })
-        el.classList.add('active');
-    })
 })
-let items = document.querySelectorAll('#menu-content > .items');
 items.forEach((el,i)=>{
     el.setAttribute('id','menu-'+i);
 })
